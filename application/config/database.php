717f162ca2f7bfb978b36691bd641409e944679a
Zakaria Naidja <zakaria.naidja@univ-constantine2.dz>
2026-01-14 03:56:15 +0100

    Fix MySQL hostname in database and tenant config

--- application/config/database.php
+++ application/config/database.php
@@ -1,5 +1,5 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
 /*
 | -------------------------------------------------------------------
@@ -74,23 +74,23 @@ $active_group = 'default';
 $query_builder = TRUE;
 
 $db['default'] = array(
-    'dsn'      => '',
-    'hostname' => 'inventorysystem-mysqlinventory-ydsxph.1.cnx3axc5l1ct6fp03g3sukqul',  // ← Changez localhost
-    'username' => 'mysql',                                                               // ← Changez root
-    'password' => 'Zakaria1304@',                                                       // ← Ajoutez le mot de passe
-    'database' => 'stock_master',                                                       // ← OK
-    'dbdriver' => 'mysqli',
-    'dbprefix' => '',
-    'pconnect' => FALSE,
-    'db_debug' => (ENVIRONMENT !== 'production'),
-    'cache_on' => FALSE,
-    'cachedir' => '',
-    'char_set' => 'utf8',
-    'dbcollat' => 'utf8_general_ci',
-    'swap_pre' => '',
-    'encrypt' => FALSE,
-    'compress' => FALSE,
-    'stricton' => FALSE,
-    'failover' => array(),
-    'save_queries' => TRUE
+	'dsn'      => '',
+	'hostname' => 'inventorysystem-mysqlinventory-ydsxph',
+	'username' => 'mysql',
+	'password' => 'Zakaria1304@',
+	'database' => 'stock_master',
+	'dbdriver' => 'mysqli',
+	'dbprefix' => '',
+	'pconnect' => FALSE,
+	'db_debug' => (ENVIRONMENT !== 'production'),
+	'cache_on' => FALSE,
+	'cachedir' => '',
+	'char_set' => 'utf8',
+	'dbcollat' => 'utf8_general_ci',
+	'swap_pre' => '',
+	'encrypt' => FALSE,
+	'compress' => FALSE,
+	'stricton' => FALSE,
+	'failover' => array(),
+	'save_queries' => TRUE
 );
