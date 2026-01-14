0f7a900257c530d18449ecf9280a98fc8ca0ffd0
Zakaria Naidja <zakaria.naidja@univ-constantine2.dz>
2026-01-14 01:26:22 +0100

    Fix base_url to production domain

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://inventorysysmulti.localhost/';
+$config['base_url'] = 'https://inventory.taqseet.shop/'; 
 $config['index_page'] = '';  // ✅ VIDE pour supprimer index.php
 
 
