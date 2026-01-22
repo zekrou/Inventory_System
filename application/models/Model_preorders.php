80dbb3c4e2ec10187a98bcb4f862eb3a405fe62f
Zakaria Naidja <zakaria.naidja@univ-constantine2.dz>
2026-01-22 12:16:04 +0100

    Fix CONCAT firstname lastname pour users

--- application/models/Model_preorders.php
+++ application/models/Model_preorders.php
@@ -1,16 +1,17 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Model_preorders extends CI_Model 
+class Model_preorders extends CI_Model
 {
     public function get_all_preorders()
     {
-        $this->db->select('p.*, u.name as customer_name, u.phone');
+        $this->db->select('p.*, CONCAT(u.firstname, " ", u.lastname) as customer_name, u.phone');
         $this->db->from('pre_orders p');
         $this->db->join('users u', 'u.id = p.user_id', 'left');
         $this->db->order_by('p.created_at', 'DESC');
         return $this->db->get()->result_array();
     }
+
 
     public function get_preorder($id)
     {
@@ -35,7 +36,7 @@ class Model_preorders extends CI_Model
         // Supprime les items d'abord
         $this->db->where('preorder_id', $id);
         $this->db->delete('pre_order_items');
-        
+
         // Puis la pré-commande
         $this->db->where('id', $id);
         $this->db->delete('pre_orders');
